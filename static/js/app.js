2c0757390394a1ec2ff7b19df6b11b6daa70ae6b
ssugiura47 <67213943+ssugiura47@users.noreply.github.com>
2020-10-23 22:56:33 -0700

    saving

--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,72 @@
-function top10dropdown(){
-    var dropdown = d3.select("#selDataset");
+
+function unpack(rows, index){
+    return rows.map(row => row[index])
+};
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+function getData(){
 
     d3.json("data/samples.json").then((data) => {
-        var sampleValues = data["samples"][0]["sample_values"]
-        var otu_ids = "OTU " + data["samples"][0]["otu_ids"]
-        var otu_labels = data["samples"][0]["otu_labels"]
-    
-        var topValues = sampleValues.sort((a, b) => b - a);
-        var topTen  = topValues.slice(0,10);
+        var allsubjects = data.metadata
+        console.log(allsubjects)
 
-    dropdown.append("option")
-        .text(topTen)
-        .property("value", topTen);
+    subjectIDs = allsubjects.map(d => d.id)
+    console.log(subjectIDs)
 
-    var data = {
-        y: otu_ids,
-        x: topTen,
-        type: "bar",
-        text: otu_labels
-    }
-    var layout = {
-        title: "Top 10 OTUs"
-    }
-    Plotly.newPlot("bar", [data], layout)
-
+    for (var i = 0; i < 153; i++){
+        var option = d3.select("#selDataset").append("option");
+        option.append("value").text(subjectIDs[i])
+        }  
     });
 };
+
+getData()
+// function table(){
+
+//     d3.json("data/samples.json").then((data) => {
+//         var sampleValues = data["samples"][0]["sample_values"]
+//         var otu_ids = "OTU " + data["samples"][0]["otu_ids"]
+//         var otu_labels = data["samples"][0]["otu_labels"]
+    
+//         var topTenValues = sampleValues.slice(0,10);
+//         var topTenIDs = otu_ids.slice(0,10);
+//         var topTenLabels = otu_labels.slice(0,10);
+    
+//     var data = {
+//     y: otu_ids,
+//     x: topTen,
+//     type: "bar",
+//     text: otu_labels
+// }
+//     var layout = {
+//     title: "Top 10 OTUs"
+// }
+//     Plotly.newPlot("bar", [data], layout)
  
-d3.json("data/samples.json").then((data) => {
-    var sampleValues = data["samples"][0]["sample_values"]
-    var otu_ids = data["samples"][0]["otu_ids"]
-    var otu_labels = data["samples"][0]["otu_labels"]
+// d3.json("data/samples.json").then((data) => {
+//     var sampleValues = data["samples"][0]["sample_values"]
+//     var otu_ids = data["samples"][0]["otu_ids"]
+//     var otu_labels = data["samples"][0]["otu_labels"]
     
-    var bubbleData = {
-        x: otu_ids,
-        y: sampleValues,
-        text: otu_labels,
-        mode: `markers`,
-        marker: {
-          size: sampleValues, 
-          color: otu_ids
-        }
-    };
+//     var bubbleData = {
+//         x: otu_ids,
+//         y: sampleValues,
+//         text: otu_labels,
+//         mode: `markers`,
+//         marker: {
+//           size: sampleValues, 
+//           color: otu_ids
+//         }
+//     };
 
-    var bubbleLayout = {
-        xaxis: {title: "OTU ID"}, 
-        title: "Belly Button Bacteria"
-      };
+//     var bubbleLayout = {
+//         xaxis: {title: "OTU ID"}, 
+//         title: "Belly Button Bacteria"
+//       };
     
-    Plotly.newPlot('bubble', [bubbleData], bubbleLayout);
-});
+//     Plotly.newPlot('bubble', [bubbleData], bubbleLayout);
+// });
 
-top10dropdown();
+// top10dropdown();
